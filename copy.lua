330ff6a92e4093eae63b195c33a2a0a5c340142e
OmegaScriptsRoblox <142178505+OmegaScriptsRoblox@users.noreply.github.com>
2023-08-13 15:58:08 -0400

    Update copy.lua

--- copy.lua
+++ copy.lua
@@ -1,1 +1,3 @@
-
+local HWID = game:GetService("RbxAnalyticsService"):GetClientId()
+game:GetService'StarterGui':SetCore("DevConsoleVisible", true)
+print("Your HWID is: " .. HWID .. ", Please screenshot and make a ticket.")
